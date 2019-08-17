0cb12646b19b9d07c52faadeba51f1083f11295d
Max Rohrer <maxprohrer@gmail.com>
2019-08-17 13:06:26 -0700

    created blog class

---
+++ src/main/java/com/max/blogAPI/Blog.java
@@ -0,0 +1,48 @@
+package com.max.blogAPI;
+
+public class Blog {
+    private int id;
+    private String title;
+    private String content;
+
+    public Blog() {}
+
+    public Blog(int id, String title, String content) {
+        this.setId(id);
+        this.setTitle(title);
+        this.setContent(content);
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+                "id = " + this.id +
+                ", title = " + this.title +
+                ", content = " + this.content +
+                "}";
+    }
+}
